a3b2816bb5d4208db6c69bfbc262747d53526107
dalalprerna88 <prerna.dalal@accenture.com>
2019-02-25 17:22:22 +0530

    Update wrapper.py

--- wrapper.py
+++ wrapper.py
@@ -4,8 +4,7 @@ import requests
 
 appName = sys.argv[1]
 appID = "1450aa62fbe21fcd180f44ed241203f4"
-token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImRhbGFscHJlcm5hODgiLCJvcmlnX2lhdCI6MTUyODE3ODQzNywidXNlcl9pZCI6NDQsImVtYWlsIjoicHJlcm5hLmRhbGFsQGFjY2VudHVyZS5jb20iLCJleHAiOjE1MjgyNjQ4Mzd9.Xxem7sAMzNJC7kH6NGUlQrE_1YVgiNDa8Guy63SHbP4"
-
+token =  "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjo0NCwiZW1haWwiOiJwcmVybmEuZGFsYWxAYWNjZW50dXJlLmNvbSIsInVzZXJuYW1lIjoiZGFsYWxwcmVybmE4OCIsImV4cCI6MTU1MTE4MjkyOCwib3JpZ19pYXQiOjE1NTEwOTY1Mjh9.P_XU6wCTw2Em72vU0whKlcEaMePnh2aq6RcDNcEjmwc"
 token = str(token).strip()
 appID = str(appID).strip()
 
@@ -24,39 +23,3 @@ headers = { "Authorization" : authtoken }
 response = requests.post(url, data=values,files=files,headers=headers)
 
 print (response.json())
-data = response.json()
-
-if data['status'] == 'Failed':
-    print(" Build Failed - "+str(data['error'])+" !")
-    exit(1)
-
-x = str(data['message']).split("=")
-x = x[1]
-
-checkurl = 'https://appsecure.accenture.com/api/executive_report/'
-values = {'appId': x, }
-authtoken = "JWT "+str(token)
-headers = {"Authorization": authtoken }
-response = requests.post(checkurl, data=values,headers=headers)
-x = response.json()
-vuldetail = x["vulnerabilitiesSummary"]
-issuefound = len(vuldetail)
-highissues = 0
-mediumissues = 0
-lowissues = 0
-while issuefound:
-    severity =  vuldetail[str(issuefound)]["severity"]
-    if severity == "High":
-        highissues = highissues+1
-    if severity == "Low":
-        lowissues = lowissues+1
-    if severity == "Medium":
-        mediumissues = mediumissues+1
-    issuefound = issuefound - 1
-if highissues > 5:
-    print("Build Failed because "+str(highissues)+" high security issues detected in your application! ")
-    exit(1)
-if highissues >=3 and mediumissues >=3:
-    print("Build Failed because "+str(highissues)+" high security issues and " +str(mediumissues)+" medium security issues detected in your application! ")
-    exit(1)
-print("Your Application has "+str(highissues)+ " high issues, "+str(mediumissues)+" medium issues, "+str(lowissues)+" low issues. ")
